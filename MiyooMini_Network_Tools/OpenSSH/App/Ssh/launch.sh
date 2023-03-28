429ca03c06cfac5a3612c27ae02edab9d4fb9811
Schmurtz <Schmurtz@gmail.com>
2023-03-28 13:26:23 +0200

    fix openssh path

---
+++ MiyooMini_Network_Tools/OpenSSH/App/Ssh/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo $0 $*
+# LD_PRELOAD=/mnt/SDCARD/miyoo/lib/libpadsp.so /mnt/SDCARD/.tmp_update/bin/infoPanel -t "Launching OpenSSH" -m "SSH server has been launched !" --auto &
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/mnt/SDCARD/App/Ssh/lib"
+
+cd $(dirname "$0")
+
+if ! [ -f /appconfigs/ssh_host_ed25519_key ] ; then
+	cp ./ssh_host_ed25519_key /appconfigs/
+	chmod 600 /appconfigs/ssh_host_ed25519_key
+fi
+
+pkill -9 sshd
+pkill -9 wpa_supplicant
+pkill -9 udhcpc
+mkdir /var/empty
+
+/mnt/SDCARD/App/Ssh/sshd -f /mnt/SDCARD/App/Ssh/sshd_config
